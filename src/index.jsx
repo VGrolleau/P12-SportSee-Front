c1e2eb7c3bc3e0693ed53cd9050527ca98fda595
VGrolleau <virginie.grolleau+github@protonmail.com>
2022-11-18 15:31:10 +0100

    Finish sidenav

--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,14 @@ import './utils/style/index.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import Dashboard from './pages/Dashboard.jsx';
+import SideNav from './components/SideNav';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Header />
+      <SideNav />
       <Routes>
         <Route index element={<Dashboard />} />
       </Routes>
